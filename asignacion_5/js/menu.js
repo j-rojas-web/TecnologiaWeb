14f59d5824bb2e9940893c4b4e6438f0be77e9f0
j-rojas-web <rojas.jarq@gmail.com>
2019-08-20 17:57:49 -0400

    adicion de galeria

--- asignacion_5/js/menu.js
+++ asignacion_5/js/menu.js
@@ -1,3 +1,13 @@
 function MenuDesplegable() {
 	document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
 }
+function Hora(){
+	var horaActual = new Date();
+
+	var hora = horaActual.getHours();
+	var minuto = horaActual.getMinutes();
+	var segundo = horaActual.getSeconds();
+
+	var imprimir = hora + ":" + minuto + ":" + segundo;
+	document.getElementById('hora').innerHTML = imprimir;
+}

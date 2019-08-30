255e23eb3823cb98801d31e6e5176f3ff54cc90e
j-rojas-web <rojas.jarq@gmail.com>
2019-08-30 10:07:25 -0400

    json Update detallesTarija

--- asignacion_10/js/weatherapi.js
+++ asignacion_10/js/weatherapi.js
@@ -1,1 +1,16 @@
 let weatherRequest = new XMLHttpRequest();
+
+let apiURLstring = "http://api.openweathermap.org/data/2.5/forecast?id=5604473&APPID=144723673b4b8f8845aafd72adae233d";
+weatherRequest.open("GET", apiURLstring, true);
+weatherRequest.send();
+ 
+weatherRequest.onload = function(){
+    let weatherData = JSON.parse(weatherRequest.responseText);
+    console.log(weatherData);
+    document.getElementById("cc-temp").innerHTML = weatherData.main.temp;
+    let icon = "http://openweathermap.org/img/wn/"+ weatherData.weather[0].icon + "font.png";
+    let desc = weatherData.weather[0].description;
+    document.getElementById("cc-img").setAttribute("src", icono);
+    document.getElementById("cc-img").setAttribute("alt", desc);
+
+}

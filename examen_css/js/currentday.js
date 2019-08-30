1781664a8ebd48e5129845705c91218e1eaa9e57
j-rojas-web <rojas.jarq@gmail.com>
2019-08-30 15:43:34 -0400

    examen update

--- examen_css/js/currentday.js
+++ examen_css/js/currentday.js
@@ -1,1 +1,15 @@
 ﻿// fecha actual dinámicamente en el siguiente formato de fecha: lunes 6 de abril de 2020
+startTime();
+checkTime();
+function startTime(){
+today=new Date();
+h=today.getHours();
+m=today.getMinutes();
+s=today.getSeconds();
+m=checkTime(m);
+s=checkTime(s);
+document.getElementById('reloj').innerHTML=h+":"+m+":"+s;
+t=setTimeout('startTime()',500);}
+function checkTime(i)
+{if (i<10) {i="0" + i;}return i;}
+window.onload=function(){startTime();}
